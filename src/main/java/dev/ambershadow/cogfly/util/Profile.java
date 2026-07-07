884edfaa03744287e70556864a1c74518a2034b6
amber <amberthepixel@gmail.com>
2026-07-07 06:58:00 +0200

    fix warnings

--- src/main/java/dev/ambershadow/cogfly/util/Profile.java
+++ src/main/java/dev/ambershadow/cogfly/util/Profile.java
@@ -9,10 +9,8 @@ import javax.swing.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public class Profile {
     List<ModData> installedMods = new ArrayList<>();
