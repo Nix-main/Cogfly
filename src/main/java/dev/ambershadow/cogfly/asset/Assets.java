f01de3f13e25c2f41ee8bf99e4a8dbbea4da1578
amber <amberthepixel@gmail.com>
2026-07-07 21:33:26 +0200

    Fix warnings

--- src/main/java/dev/ambershadow/cogfly/asset/Assets.java
+++ src/main/java/dev/ambershadow/cogfly/asset/Assets.java
@@ -9,9 +9,9 @@ public class Assets {
     public static CogflyAsset icon = getAsset("icon.png");
     public static CogflyAsset openSaves = getAsset("openSaves.png");
     public static CogflyAsset centralIcon = getAsset("cogfly_art.png");
-    private static CogflyAsset discord = getAsset("Discord-Symbol-White.svg");
-    private static CogflyAsset github = getAsset("GitHub_Invertocat_White.svg");
-    private static CogflyAsset patreon = getAsset("PATREON_SYMBOL_1_WHITE_RGB.svg");
+    private static final CogflyAsset discord = getAsset("Discord-Symbol-White.svg");
+    private static final CogflyAsset github = getAsset("GitHub_Invertocat_White.svg");
+    private static final CogflyAsset patreon = getAsset("PATREON_SYMBOL_1_WHITE_RGB.svg");
     public static CogflyAsset[] linkIcons = {discord, github, patreon};
     public static CogflyAsset silksongIcon = getAsset("silksong64x64.png");
 }
