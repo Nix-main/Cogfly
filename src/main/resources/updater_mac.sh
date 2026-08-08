a7cd4d98edb480bde025c9628f04fd2aeed5253f
amber <thenixpart@gmail.com>
2026-08-08 03:13:54 -0400

    add back mac hash check, lower version

--- src/main/resources/updater_mac.sh
+++ src/main/resources/updater_mac.sh
@@ -10,8 +10,8 @@ echo "Downloading..."
 dmg=$(mktemp).dmg
 curl -L "$url" -o "$dmg"
 
-#echo "Checking hash..."
-#[ "$(shasum -a 256 "$dmg" | cut -d' ' -f1)" = "$sha" ] || exit 1
+echo "Checking hash..."
+[ "$(shasum -a 256 "$dmg" | cut -d' ' -f1)" = "$sha" ] || exit 1
 
 echo "Installing..."
 mnt=$(mktemp -d)
